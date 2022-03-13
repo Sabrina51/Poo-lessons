902fdb06a6ddbc332f383a5776f666a81cca76bb
Sabrina Silva <sabrina.silva51@fatec.sp.gov.br>
2022-03-13 01:37:16 -0300

    first commit

---
+++ cartesiano/src/cartesiano/Cartesiano.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cartesiano;
+
+/**
+ *
+ * @author Sabrina Silva
+ */
+public class Cartesiano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
